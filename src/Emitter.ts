2b6b848bab49a4285f3bdf8aa385eb4204038707
ZeroUltimax <antoinerobertson92@gmail.com>
2021-05-26 17:38:18 -0400

    Typed emitter

---
+++ src/Emitter.ts
@@ -0,0 +1,24 @@
+type EventMap<T = any> = {
+  [k in keyof T]: T[k] extends any[] ? T[k] : never;
+};
+
+export class Emitter<M extends EventMap = any> {
+  private eventsMap: { [E in keyof M]?: ((...args: M[E]) => void)[] } = {};
+
+  public on<E extends keyof M>(event: E, cb: (...args: M[E]) => void) {
+    this.eventsMap[event] = [...(this.eventsMap[event] || [])!, cb];
+  }
+
+  public off<E extends keyof M>(event: E, cb: (...args: M[E]) => void) {
+    const events = (this.eventsMap[event] || [])!.filter((x) => x !== cb);
+    if (events.length) {
+      this.eventsMap[event] = events;
+    } else {
+      delete this.eventsMap[event];
+    }
+  }
+
+  public emit<E extends keyof M>(event: E, ...args: M[E]) {
+    this.eventsMap[event]?.forEach((cb) => cb(...args));
+  }
+}
